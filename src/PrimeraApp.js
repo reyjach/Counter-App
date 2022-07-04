4a356ef4e2f935d73b40b0ae1279b3b8582798d4
Andres Martinez <reymuerto14@hotmail.com>
2022-07-04 02:48:52 -0500

    se elimino todo lo que tenia pordefecto en src, se creo un index, un index css y un componente

---
+++ src/PrimeraApp.js
@@ -0,0 +1,14 @@
+import React from 'react';
+//import React, { Fragment } from 'react';
+
+const PrimeraApp = () => {
+
+    return (
+        <>
+            <h1>Hola mundo</h1>
+            <p>Mi primera App</p>
+        </>
+    );
+};
+
+export default PrimeraApp;
