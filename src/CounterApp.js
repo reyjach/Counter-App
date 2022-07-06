7a910c5acfdf2494fa16d9f84989f13b0df9dde7
Andres Martinez <reymuerto14@hotmail.com>
2022-07-06 01:06:39 -0500

    se creo un useState y handleSubtract

--- src/CounterApp.js
+++ src/CounterApp.js
@@ -1,17 +1,30 @@
-import React from 'react';
+import React, {useState} from 'react';
 import ProTypes from 'prop-types';
 
-const CounterApp = ({ value }) => {
+const CounterApp = ({ value = 10 }) => {
 
-    const handleApp = (e) => {
-        console.log(e)
+    const [ counter, setCounter ] = useState( value );
+
+    const handleApp = () => {
+        setCounter( counter + 1 );
+        //setCounter( (c) => c + 1 );
+    }
+
+    const handleMin = () => {
+        setCounter(counter -1);
+    }
+
+    const handleRes = () => {
+        setCounter(value);
     }
 
     return (
         <>  
             <h1>CounterApp</h1>
-            <h2>{value}</h2>
+            <h2>{counter}</h2>
             <button onClick={ handleApp }>+1</button>
+            <button onClick={ handleRes }>Reset</button>
+            <button onClick={ handleMin }>-1</button>
         </>
 
     )
